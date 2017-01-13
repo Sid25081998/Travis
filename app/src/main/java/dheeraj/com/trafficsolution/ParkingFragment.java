0cb52bbe5aeae28859c1255a1100755164855047
prasang7 <prasang.sharma7@gmail.com>
2017-01-13 20:38:17 +0530

    Parking Finder Module UI ready

--- app/src/main/java/dheeraj/com/trafficsolution/ParkingFragment.java
+++ app/src/main/java/dheeraj/com/trafficsolution/ParkingFragment.java
@@ -1,6 +1,7 @@
 package dheeraj.com.trafficsolution;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -13,7 +14,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by prasang7 on 28/12/16.
@@ -21,7 +25,8 @@ import android.widget.Toast;
 public class ParkingFragment extends Fragment {
 
     View view;
-    Button find_2wheeler_Parking, find_4wheeler_Parking;
+    Button bt_find_2wheeler_Parking, bt_find_4wheeler_Parking;
+    TextView tv_title, tv_content;
 
     public ParkingFragment() {
         // Required empty public constructor
@@ -38,7 +43,7 @@ public class ParkingFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_parking, container, false);
         init();
 
-        find_2wheeler_Parking.setOnClickListener(new View.OnClickListener() {
+        bt_find_2wheeler_Parking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getActivity(), ParkingSearchResult.class);
@@ -47,7 +52,7 @@ public class ParkingFragment extends Fragment {
             }
         });
 
-        find_4wheeler_Parking.setOnClickListener(new View.OnClickListener() {
+        bt_find_4wheeler_Parking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getActivity(), ParkingSearchResult.class);
@@ -60,7 +65,20 @@ public class ParkingFragment extends Fragment {
     }
 
     void init() {
-        find_2wheeler_Parking = (Button) view.findViewById(R.id.bt_find_2wheeler_parking);
-        find_4wheeler_Parking = (Button) view.findViewById(R.id.bt_find_4wheeler_parking);
+        bt_find_2wheeler_Parking = (Button) view.findViewById(R.id.bt_find_2wheeler_parking);
+        bt_find_4wheeler_Parking = (Button) view.findViewById(R.id.bt_find_4wheeler_parking);
+
+        tv_title = (TextView)view.findViewById(R.id.tv_parkingFinder_title);
+        tv_content = (TextView)view.findViewById(R.id.tv_parkingFinder_content);
+
+        Typeface MontReg = Typeface.createFromAsset(getActivity().getApplication().getAssets(), "Montserrat-Regular.otf");
+        Typeface MontBold = Typeface.createFromAsset(getActivity().getApplication().getAssets(), "Montserrat-Bold.otf");
+        //Typeface MontHair = Typeface.createFromAsset(getApplication().getAssets(), "Montserrat-Hairline.otf");
+
+        bt_find_2wheeler_Parking.setTypeface(MontBold);
+        bt_find_4wheeler_Parking.setTypeface(MontBold);
+
+        tv_title.setTypeface(MontBold);
+        tv_content.setTypeface(MontReg);
     }
 }
